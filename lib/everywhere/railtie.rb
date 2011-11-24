d9ad1f95f1bd9111b7a6c9e484f6ed11f4e2359b
Akira Matsuda <ronnie@dio.jp>
2011-11-24 18:33:23 +0900

    make syntax configurable

--- lib/everywhere/railtie.rb
+++ lib/everywhere/railtie.rb
@@ -1,13 +1,16 @@
 require 'active_record'
 
+module ActiveRecord
+  class Base
+    cattr_accessor :where_syntax, :instance_writer => false
+  end
+end
+
 module Everywhere
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'everywhere' do |app|
       ActiveSupport.on_load(:active_record) do
-#         require 'everywhere/symbol'
-#         require 'everywhere/method'
-#         require 'everywhere/hash_key'
-        require 'everywhere/hash_value'
+        require "everywhere/#{app.config.active_record.where_syntax || 'hash_value'}"
       end
     end
   end
