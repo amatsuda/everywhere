2acefea82236e97d6a6318465ad949c625c7b091
Akira Matsuda <ronnie@dio.jp>
2011-11-25 17:07:50 +0900

    where :foo => {:not_like => bar}

--- lib/everywhere/hash_value.rb
+++ lib/everywhere/hash_value.rb
@@ -9,7 +9,7 @@ module ActiveRecord
         def build_from_hash_with_not(engine, attributes, default_table)
           attributes_with_not = attributes.map do |column, value|
             # {key: {not: value}}
-            if value.is_a?(Hash) && (value.keys.size == 1) && value.keys.first.in?([:not, :like])
+            if value.is_a?(Hash) && (value.keys.size == 1) && value.keys.first.in?([:not, :like, :not_like])
               ["#{column}__#{value.keys.first}__", value.values.first]
             else
               [column, value]
@@ -22,6 +22,9 @@ module ActiveRecord
             elsif rel.left.name.to_s.ends_with?('__like__')
               rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
               Arel::Nodes::Matches.new rel.left, rel.right
+            elsif rel.left.name.to_s.ends_with?('__not_like__')
+              rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
+              Arel::Nodes::DoesNotMatch.new rel.left, rel.right
             else
               rel
             end
@@ -35,7 +38,7 @@ module ActiveRecord
       def build_from_hash_with_not(attributes, default_table)
         attributes_with_not = attributes.map do |column, value|
           # {key: {not: value}}
-          if value.is_a?(Hash) && (value.keys.size == 1) && ((value.keys.first == :not) || (value.keys.first == :like))
+          if value.is_a?(Hash) && (value.keys.size == 1) && ((value.keys.first == :not) || (value.keys.first == :like) || (value.keys.first == :not_like))
             ["#{column}__#{value.keys.first}__", value.values.first]
           else
             [column, value]
@@ -48,6 +51,9 @@ module ActiveRecord
           elsif rel.left.name.to_s.ends_with?('__like__')
             rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
             Arel::Nodes::Matches.new rel.left, rel.right
+          elsif rel.left.name.to_s.ends_with?('__not_like__')
+            rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
+            Arel::Nodes::DoesNotMatch.new rel.left, rel.right
           else
             rel
           end
