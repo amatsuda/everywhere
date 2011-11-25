ed9b81abafabef8bf88f648f6a6768b85bc3d2fa
Akira Matsuda <ronnie@dio.jp>
2011-11-25 15:33:25 +0900

    where_like :foo => bar

--- lib/everywhere/method.rb
+++ lib/everywhere/method.rb
@@ -3,7 +3,7 @@ require 'everywhere/util'
 module ActiveRecord
   class Base
     class << self
-      delegate :where_not, :to => :scoped
+      delegate :where_not, :where_like, :to => :scoped
     end
   end
 
@@ -15,6 +15,14 @@ module ActiveRecord
 
       relation = clone
       relation.where_values += build_where(opts, rest).map {|r| negate r}
+      relation
+    end
+
+    def where_like(opts, *rest)
+      return self if opts.blank?
+
+      relation = clone
+      relation.where_values += build_where(opts, rest).map {|r| Arel::Nodes::Matches.new r.left, r.right}
       relation
     end
   end
