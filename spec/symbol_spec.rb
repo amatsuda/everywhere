c913d6decabbd1ac5e10b26b3098bc3817a7627b
Akira Matsuda <ronnie@dio.jp>
2011-11-25 15:13:44 +0900

    spec

--- spec/symbol_spec.rb
+++ spec/symbol_spec.rb
@@ -11,42 +11,44 @@ describe 'normal query' do
   its(:to_sql) { should == %q["posts"."name" = 'hello'] }
 end
 
-describe 'not eq' do
-  before do
-    @where = Post.where(:not, :name => 'hello').where_values
+describe 'not' do
+  describe 'not eq' do
+    before do
+      @where = Post.where(:not, :name => 'hello').where_values
+    end
+    subject { @where }
+    it { @where.should have(1).item }
+    subject { @where.first }
+    its(:to_sql) { should == %q["posts"."name" != 'hello'] }
   end
-  subject { @where }
-  it { @where.should have(1).item }
-  subject { @where.first }
-  its(:to_sql) { should == %q["posts"."name" != 'hello'] }
-end
 
-describe 'not null' do
-  before do
-    @where = Post.where(:not, :created_at => nil).where_values
+  describe 'not null' do
+    before do
+      @where = Post.where(:not, :created_at => nil).where_values
+    end
+    subject { @where }
+    it { @where.should have(1).item }
+    subject { @where.first }
+    its(:to_sql) { should == %q["posts"."created_at" IS NOT NULL] }
   end
-  subject { @where }
-  it { @where.should have(1).item }
-  subject { @where.first }
-  its(:to_sql) { should == %q["posts"."created_at" IS NOT NULL] }
-end
 
-describe 'not in' do
-  before do
-    @where = Post.where(:not, :name => %w[hello goodbye]).where_values
+  describe 'not in' do
+    before do
+      @where = Post.where(:not, :name => %w[hello goodbye]).where_values
+    end
+    subject { @where }
+    it { @where.should have(1).item }
+    subject { @where.first }
+    its(:to_sql) { should == %q["posts"."name" NOT IN ('hello', 'goodbye')] }
   end
-  subject { @where }
-  it { @where.should have(1).item }
-  subject { @where.first }
-  its(:to_sql) { should == %q["posts"."name" NOT IN ('hello', 'goodbye')] }
-end
 
-describe 'association' do
-  before do
-    @where = Post.joins(:comments).where(:not, :comments => {:body => 'foo'}).where_values
+  describe 'association' do
+    before do
+      @where = Post.joins(:comments).where(:not, :comments => {:body => 'foo'}).where_values
+    end
+    subject { @where }
+    it { @where.should have(1).item }
+    subject { @where.first }
+    its(:to_sql) { should == %q["comments"."body" != 'foo'] }
   end
-  subject { @where }
-  it { @where.should have(1).item }
-  subject { @where.first }
-  its(:to_sql) { should == %q["comments"."body" != 'foo'] }
 end
