67c0d640d696d65e09b4876ce2d4d8e6ab16332f
Akira Matsuda <ronnie@dio.jp>
2012-06-15 17:17:25 +0900

    AR 3.2.4+, 3.1.5+, and 3.0.13+ ready
    `build_from_hash` method arity has been changed for CVE-2012-2661 fix

--- lib/everywhere/hash_key.rb
+++ lib/everywhere/hash_key.rb
@@ -6,11 +6,78 @@ module ActiveRecord
       class << self
         include Everywhere::Util
 
-        def build_from_hash_with_not_and_like_and_not_like(engine, attributes, default_table)
+        # >= 3.2.4, >= 3.1.5
+        if ActiveRecord::PredicateBuilder.method(:build_from_hash).arity == -4
+          def build_from_hash_with_not_and_like_and_not_like(engine, attributes, default_table, allow_table_name = true)
+            attributes_with_not_and_like_and_not_like = {}
+            attributes.each do |column, value|
+              # {not: {key: value}}
+              if column.in?([:not, :like, :not_like])
+                value.each do |k, v|
+                  attributes_with_not_and_like_and_not_like["#{k}__#{column}__"] = v
+                end
+              else
+                attributes_with_not_and_like_and_not_like[column] = value
+              end
+            end
+            build_from_hash_without_not_and_like_and_not_like(engine, attributes_with_not_and_like_and_not_like, default_table, allow_table_name).map do |rel|
+              if rel.left.name.to_s.ends_with? '__not__'
+                rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
+                negate rel
+              elsif rel.left.name.to_s.ends_with? '__like__'
+                rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
+                Arel::Nodes::Matches.new rel.left, rel.right
+              elsif rel.left.name.to_s.ends_with? '__not_like__'
+                rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
+                Arel::Nodes::DoesNotMatch.new rel.left, rel.right
+              else
+                rel
+              end
+            end
+          end
+        # < 3.2.4, < 3.1.5, >= 4.0?
+        else
+          def build_from_hash_with_not_and_like_and_not_like(engine, attributes, default_table)
+            attributes_with_not_and_like_and_not_like = {}
+            attributes.each do |column, value|
+              # {not: {key: value}}
+              if column.in?([:not, :like, :not_like])
+                value.each do |k, v|
+                  attributes_with_not_and_like_and_not_like["#{k}__#{column}__"] = v
+                end
+              else
+                attributes_with_not_and_like_and_not_like[column] = value
+              end
+            end
+            build_from_hash_without_not_and_like_and_not_like(engine, attributes_with_not_and_like_and_not_like, default_table).map do |rel|
+              if rel.left.name.to_s.ends_with? '__not__'
+                rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
+                negate rel
+              elsif rel.left.name.to_s.ends_with? '__like__'
+                rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
+                Arel::Nodes::Matches.new rel.left, rel.right
+              elsif rel.left.name.to_s.ends_with? '__not_like__'
+                rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
+                Arel::Nodes::DoesNotMatch.new rel.left, rel.right
+              else
+                rel
+              end
+            end
+          end
+        end
+        alias_method_chain :build_from_hash, :not_and_like_and_not_like
+      end
+    # 3.0
+    else
+      include Everywhere::Util
+
+      # >= 3.0.13
+      if ActiveRecord::PredicateBuilder.method(:build_from_hash).arity == -3
+        def build_from_hash_with_not_and_like_and_not_like(attributes, default_table, allow_table_name = true)
           attributes_with_not_and_like_and_not_like = {}
           attributes.each do |column, value|
             # {not: {key: value}}
-            if column.in?([:not, :like, :not_like])
+            if (column == :not) || (column == :like) || (column == :not_like)
               value.each do |k, v|
                 attributes_with_not_and_like_and_not_like["#{k}__#{column}__"] = v
               end
@@ -18,7 +85,7 @@ module ActiveRecord
               attributes_with_not_and_like_and_not_like[column] = value
             end
           end
-          build_from_hash_without_not_and_like_and_not_like(engine, attributes_with_not_and_like_and_not_like, default_table).map do |rel|
+          build_from_hash_without_not_and_like_and_not_like(attributes_with_not_and_like_and_not_like, default_table, allow_table_name).map do |rel|
             if rel.left.name.to_s.ends_with? '__not__'
               rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
               negate rel
@@ -33,35 +100,33 @@ module ActiveRecord
             end
           end
         end
-        alias_method_chain :build_from_hash, :not_and_like_and_not_like
-      end
-    else
-      include Everywhere::Util
-
-      def build_from_hash_with_not_and_like_and_not_like(attributes, default_table)
-        attributes_with_not_and_like_and_not_like = {}
-        attributes.each do |column, value|
-          # {not: {key: value}}
-          if (column == :not) || (column == :like) || (column == :not_like)
-            value.each do |k, v|
-              attributes_with_not_and_like_and_not_like["#{k}__#{column}__"] = v
+      # < 3.0.13
+      else
+        def build_from_hash_with_not_and_like_and_not_like(attributes, default_table)
+          attributes_with_not_and_like_and_not_like = {}
+          attributes.each do |column, value|
+            # {not: {key: value}}
+            if (column == :not) || (column == :like) || (column == :not_like)
+              value.each do |k, v|
+                attributes_with_not_and_like_and_not_like["#{k}__#{column}__"] = v
+              end
+            else
+              attributes_with_not_and_like_and_not_like[column] = value
             end
-          else
-            attributes_with_not_and_like_and_not_like[column] = value
           end
-        end
-        build_from_hash_without_not_and_like_and_not_like(attributes_with_not_and_like_and_not_like, default_table).map do |rel|
-          if rel.left.name.to_s.ends_with? '__not__'
-            rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
-            negate rel
-          elsif rel.left.name.to_s.ends_with? '__like__'
-            rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
-            Arel::Nodes::Matches.new rel.left, rel.right
-          elsif rel.left.name.to_s.ends_with? '__not_like__'
-            rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
-            Arel::Nodes::DoesNotMatch.new rel.left, rel.right
-          else
-            rel
+          build_from_hash_without_not_and_like_and_not_like(attributes_with_not_and_like_and_not_like, default_table).map do |rel|
+            if rel.left.name.to_s.ends_with? '__not__'
+              rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
+              negate rel
+            elsif rel.left.name.to_s.ends_with? '__like__'
+              rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
+              Arel::Nodes::Matches.new rel.left, rel.right
+            elsif rel.left.name.to_s.ends_with? '__not_like__'
+              rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
+              Arel::Nodes::DoesNotMatch.new rel.left, rel.right
+            else
+              rel
+            end
           end
         end
       end
--- lib/everywhere/hash_value.rb
+++ lib/everywhere/hash_value.rb
@@ -6,16 +6,76 @@ module ActiveRecord
       class << self
         include Everywhere::Util
 
-        def build_from_hash_with_not_and_like_and_not_like(engine, attributes, default_table)
+        # >= 3.2.4, >= 3.1.5
+        if ActiveRecord::PredicateBuilder.method(:build_from_hash).arity == -4
+          def build_from_hash_with_not_and_like_and_not_like(engine, attributes, default_table, allow_table_name = true)
+            attributes_with_not_and_like_and_not_like = attributes.map do |column, value|
+              # {key: {not: value}}
+              if value.is_a?(Hash) && (value.keys.size == 1) && value.keys.first.in?([:not, :like, :not_like])
+                ["#{column}__#{value.keys.first}__", value.values.first]
+              else
+                [column, value]
+              end
+            end
+            build_from_hash_without_not_and_like_and_not_like(engine, attributes_with_not_and_like_and_not_like, default_table, allow_table_name).map do |rel|
+              if rel.left.name.to_s.ends_with?('__not__')
+                rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
+                negate rel
+              elsif rel.left.name.to_s.ends_with?('__like__')
+                rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
+                Arel::Nodes::Matches.new rel.left, rel.right
+              elsif rel.left.name.to_s.ends_with?('__not_like__')
+                rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
+                Arel::Nodes::DoesNotMatch.new rel.left, rel.right
+              else
+                rel
+              end
+            end
+          end
+        # < 3.2.4, < 3.1.5, >= 4.0?
+        else
+          def build_from_hash_with_not_and_like_and_not_like(engine, attributes, default_table)
+            attributes_with_not_and_like_and_not_like = attributes.map do |column, value|
+              # {key: {not: value}}
+              if value.is_a?(Hash) && (value.keys.size == 1) && value.keys.first.in?([:not, :like, :not_like])
+                ["#{column}__#{value.keys.first}__", value.values.first]
+              else
+                [column, value]
+              end
+            end
+            build_from_hash_without_not_and_like_and_not_like(engine, attributes_with_not_and_like_and_not_like, default_table).map do |rel|
+              if rel.left.name.to_s.ends_with?('__not__')
+                rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
+                negate rel
+              elsif rel.left.name.to_s.ends_with?('__like__')
+                rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
+                Arel::Nodes::Matches.new rel.left, rel.right
+              elsif rel.left.name.to_s.ends_with?('__not_like__')
+                rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
+                Arel::Nodes::DoesNotMatch.new rel.left, rel.right
+              else
+                rel
+              end
+            end
+          end
+        end
+        alias_method_chain :build_from_hash, :not_and_like_and_not_like
+      end
+    else
+      include Everywhere::Util
+
+      # >= 3.0.13
+      if ActiveRecord::PredicateBuilder.method(:build_from_hash).arity == -3
+        def build_from_hash_with_not_and_like_and_not_like(attributes, default_table, allow_table_name = true)
           attributes_with_not_and_like_and_not_like = attributes.map do |column, value|
             # {key: {not: value}}
-            if value.is_a?(Hash) && (value.keys.size == 1) && value.keys.first.in?([:not, :like, :not_like])
+            if value.is_a?(Hash) && (value.keys.size == 1) && ((value.keys.first == :not) || (value.keys.first == :like) || (value.keys.first == :not_like))
               ["#{column}__#{value.keys.first}__", value.values.first]
             else
               [column, value]
             end
           end
-          build_from_hash_without_not_and_like_and_not_like(engine, attributes_with_not_and_like_and_not_like, default_table).map do |rel|
+          build_from_hash_without_not_and_like_and_not_like(attributes_with_not_and_like_and_not_like, default_table, allow_table_name).map do |rel|
             if rel.left.name.to_s.ends_with?('__not__')
               rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
               negate rel
@@ -30,32 +90,30 @@ module ActiveRecord
             end
           end
         end
-        alias_method_chain :build_from_hash, :not_and_like_and_not_like
-      end
-    else
-      include Everywhere::Util
-
-      def build_from_hash_with_not_and_like_and_not_like(attributes, default_table)
-        attributes_with_not_and_like_and_not_like = attributes.map do |column, value|
-          # {key: {not: value}}
-          if value.is_a?(Hash) && (value.keys.size == 1) && ((value.keys.first == :not) || (value.keys.first == :like) || (value.keys.first == :not_like))
-            ["#{column}__#{value.keys.first}__", value.values.first]
-          else
-            [column, value]
+      # < 3.0.13
+      else
+        def build_from_hash_with_not_and_like_and_not_like(attributes, default_table)
+          attributes_with_not_and_like_and_not_like = attributes.map do |column, value|
+            # {key: {not: value}}
+            if value.is_a?(Hash) && (value.keys.size == 1) && ((value.keys.first == :not) || (value.keys.first == :like) || (value.keys.first == :not_like))
+              ["#{column}__#{value.keys.first}__", value.values.first]
+            else
+              [column, value]
+            end
           end
-        end
-        build_from_hash_without_not_and_like_and_not_like(attributes_with_not_and_like_and_not_like, default_table).map do |rel|
-          if rel.left.name.to_s.ends_with?('__not__')
-            rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
-            negate rel
-          elsif rel.left.name.to_s.ends_with?('__like__')
-            rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
-            Arel::Nodes::Matches.new rel.left, rel.right
-          elsif rel.left.name.to_s.ends_with?('__not_like__')
-            rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
-            Arel::Nodes::DoesNotMatch.new rel.left, rel.right
-          else
-            rel
+          build_from_hash_without_not_and_like_and_not_like(attributes_with_not_and_like_and_not_like, default_table).map do |rel|
+            if rel.left.name.to_s.ends_with?('__not__')
+              rel.left.name = rel.left.name.to_s.sub(/__not__$/, '').to_sym
+              negate rel
+            elsif rel.left.name.to_s.ends_with?('__like__')
+              rel.left.name = rel.left.name.to_s.sub(/__like__$/, '').to_sym
+              Arel::Nodes::Matches.new rel.left, rel.right
+            elsif rel.left.name.to_s.ends_with?('__not_like__')
+              rel.left.name = rel.left.name.to_s.sub(/__not_like__$/, '').to_sym
+              Arel::Nodes::DoesNotMatch.new rel.left, rel.right
+            else
+              rel
+            end
           end
         end
       end
